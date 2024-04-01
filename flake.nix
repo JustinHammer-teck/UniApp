2e6171461d61e473f5beb822fb5a2a14a4b3a501
Moritz Zimmermann <moritzzmn@Moritzs-MacBook-Pro.local>
2024-04-01 14:47:20 +1100

    UniApp: construct project

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description =
+    "A Nix-flake-based python development environment - this only fit with my Intel-base MacBook";
+
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable"; };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      system = "x86_64-darwin";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.x86_64-darwin.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [ pkgs.python3 ];
+
+        shellHook = ''
+          echo "hello to python dev shell"  
+        '';
+
+      };
+    };
+}
---
+++ main.py
@@ -0,0 +1,31 @@
+class Sumthing:
+    name: str
+    age: int
+    subjects: array[]
+
+    def __init__(self, name: str, age: int, subjects: array[str]):
+        self.name = name
+        self.age = age
+        self.subjects = subjects
+
+
+def main():
+
+    myObj = Sumthing("Jason", 40, ["db", "architecture"])
+
+    file = open("./student.data", "wb")
+
+    file.write(myObj.encode())
+
+    file.close()
+
+    file = open("./student.data", "rb")
+
+    result = file.read()
+    print(result.decode("utf-8"))
+
+    file.close()
+
+
+if __name__ == "__main__":
+    main()
